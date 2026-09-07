4d5d0d74530311bef3ce3dbde604d315101da59f
cath365 <inambaocatherine6@gmail.com>
2026-09-07 03:32:54 -0700

    data: remove unsupported default experience claims

--- src/lib/experience.tsx
+++ src/lib/experience.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { createContext, useContext, useState, useEffect, ReactNode } from 'react'
+import { createContext, useContext, useState, useEffect, ReactNode } from 'react'\nimport { professionalRoles } from '@/data/portfolio'
 
 export interface Experience {
   id: string
