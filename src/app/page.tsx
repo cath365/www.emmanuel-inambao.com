272823609def878c4391c34c080c23d9ba487d1d
Emmanuel inambao <inambaocatherine6@gmail.com>
2026-09-21 14:10:18 -0700

    Move engineering evidence earlier in homepage narrative

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
       <SectionViewTracker />
       <Hero />
       <ClientLogos />
-      <About />
       <Projects />
+      <About />
       <EngineeringCTA />
       <Skills />
       <GitHubContributions username="cath365" />
